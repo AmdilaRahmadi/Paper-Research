11711f9941cd0578336c4872455adb6aada22ddb
pIp <106318790+AmdilaRahmadi@users.noreply.github.com>
2024-12-10 07:12:00 +0800

    Gradle apdet

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,10 @@
+package com.example;
+
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Paper Search");
+            JFrame frame = new JFrame("Paper ReSearch");
             Gui gui = new Gui();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(800, 600);
